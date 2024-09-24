bb338eb3ada1e5305145cf69522bd800565a5825
Thrinky_asus <ptrenchs@gmail.com>
2024-09-24 19:01:34 +0200

    Se ha actualizado el .py

--- B1-Blanca-Analisis de les propietats dels papers/practicas_laboratorio/funciones.py
+++ B1-Blanca-Analisis de les propietats dels papers/practicas_laboratorio/funciones.py
@@ -417,6 +417,8 @@ def ejercicio_cristina(ruta, cifras_sig = 3, separador_decimales = '.', left = '
     texto_main += crear_input(carpeta_latex, 'Objetivo y principio de la práctica') + '\n'
     texto_main += crear_input(carpeta_latex, 'Muestra papel')+ '\n'
     texto_main += crear_input(carpeta_latex, 'Normas a considerar')+ '\n'
+    texto_main += crear_input(carpeta_latex, 'Aparatos utilizados')+ '\n'
+    texto_main += crear_input(carpeta_latex, 'Condiciones ambientales')+ '\n'
     texto_main += crear_input(carpeta_latex, 'Medidas')+ '\n'
     
     _,tabla_Grubbs = leer_tabla(ruta = 'https://raw.githubusercontent.com/ptrenchs/Master_paper/main/B1-Blanca-Analisis%20de%20les%20propietats%20dels%20papers/practicas_laboratorio/tabla_Grubbs.xlsx' , nombre = 'tabla 1')
@@ -447,7 +449,7 @@ def ejercicio_cristina(ruta, cifras_sig = 3, separador_decimales = '.', left = '
         with open(carpeta_latex + '/' + nombre_inicio.replace(' ','_')+'.tex', 'w', encoding='utf-8') as archivo:
             archivo.write(texto)
         display(tabla_latex)
-        
+    texto_main += crear_input(carpeta_latex, 'Observaciones')+ '\n'
     crear_main_latex(carpeta_latex, texto_main, left = left, center = center, right = right)
 
 
