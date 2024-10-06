006c25c7b22dc77b5a2500af469109f0625453f8
Thrinky <ptrenchs@gmail.com>
2024-10-06 14:50:51 +0200

    Se ha actualizado

--- B1-Blanca-Analisis de les propietats dels papers/practicas_laboratorio/funciones.py
+++ B1-Blanca-Analisis de les propietats dels papers/practicas_laboratorio/funciones.py
@@ -395,7 +395,9 @@ def ejercicio_blanca(ruta, num_mostres = 5, cifras_sig = 3, separador_decimales 
             for pos, lista in enumerate(new_valores):
                 val_provis = [ls for ls in lista if abs(ls-medias[pos])/val_std[pos] < num_g  or isnan(ls)] # or not isnan(abs(ls-medias[pos])/val_std[pos])
                 if len(val_provis) != len(new_valores[pos]) and bucle:
+                    print([abs(ls-medias[pos])/val_std[pos] for ls in lista],num_g)
                     new_valores[pos] = [ls if abs(ls-medias[pos])/val_std[pos] < num_g else np.nan for ls in lista]
+
                     bucle_end = False
             if bucle_end:
                 break
